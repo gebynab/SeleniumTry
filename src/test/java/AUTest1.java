987a340844a0d3f43c4df75c02e913f44c56ecf5
gebynab <nabilageby@gmail.com>
2023-02-14 10:59:04 +0700

    AUTest1.java

--- src/test/java/AUTest1.java
+++ src/test/java/AUTest1.java
@@ -3,40 +3,54 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/EmptyTestNGTest.java to edit this template
  */
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.Assert;
 import static org.testng.Assert.*;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 /**
  *
  * @author PLUTO5
  */
 public class AUTest1 {
+WebDriver driver = null;
     
-    public AUTest1() {
+    @BeforeTest
+    public void setUp (){
+        WebDriverManager.firefoxdriver().setup();
+        FirefoxBinary firefoxBinary = new FirefoxBinary();
+        FirefoxOptions options = new FirefoxOptions();
+        options.setBinary(firefoxBinary);
+        options.setHeadless(true);  // <-- headless set here
+        driver = new FirefoxDriver(options);
     }
-
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
-
-    @BeforeClass
-    public static void setUpClass() throws Exception {
+    
+        @Test
+    public void firstTry(){
+        driver.get("http://localhost/AUTng-main/index.php");
+        driver.findElement(By.id("username")).sendKeys("admin");
+        driver.findElement(By.id("password")).sendKeys("admin123");
+        driver.findElement(By.xpath("/html/body/div/form/input[3]")).click();
+        String expectedTittle = "Welcome back, admin!";
+        String actualTittle = driver.findElement(By.xpath("/html/body/div/p")).getText();
+        Assert.assertEquals(actualTittle,expectedTittle);
     }
-
-    @AfterClass
-    public static void tearDownClass() throws Exception {
-    }
-
-    @BeforeMethod
-    public void setUpMethod() throws Exception {
-    }
-
-    @AfterMethod
-    public void tearDownMethod() throws Exception {
+    
+    @AfterTest
+    public void tearDown(){
+        driver.close();
+        //driver.quit();
     }
 }
